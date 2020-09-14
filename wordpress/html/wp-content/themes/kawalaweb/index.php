337b49051eed5dfcab8d251867974541c31b96f0
kwrsin <kwrsin@gmail.com>
2020-09-14 20:32:06 +0900

    kawalaweb initial commit.

---
+++ wordpress/html/wp-content/themes/kawalaweb/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello World!!<br>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque dignissimos impedit officiis dolorem voluptate. Mollitia perferendis fuga inventore quasi architecto excepturi quibusdam ex delectus sint. Totam reprehenderit eveniet inventore doloremque?
+    Necessitatibus tempore facilis id voluptas accusantium omnis deserunt ea nam, earum harum eos aperiam natus quasi maiores praesentium. Quod inventore pariatur sapiente facilis commodi omnis. Tempora sapiente voluptas magni laudantium?
+    Asperiores minima dolor odio dicta velit non voluptatibus laborum quo illo cupiditate doloribus amet laboriosam est quasi obcaecati corporis, rem nisi aliquam recusandae tempore voluptatum, cumque delectus? In, enim ex?
+    Voluptatibus quam sint tenetur, nihil dolorem excepturi maxime harum corporis tempora totam nobis dolorum quasi unde adipisci molestias consectetur culpa ab deleniti veniam eius doloremque praesentium! A, iusto totam! Amet!
+    Earum tenetur quaerat voluptas fuga maiores dolorem, ab eius cum temporibus nam. Repellendus libero expedita, pariatur inventore aliquam esse animi dicta aperiam odit, sint officiis iusto quas mollitia quaerat accusantium?
+</body>
+</html>
