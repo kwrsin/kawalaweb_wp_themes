1bd28528f9e37614d7598e3f57ff1a5b34157f66
kwrsin <kwrsin@gmail.com>
2020-09-30 11:25:01 +0900

    loading stylesheets

---
+++ wordpress/html/wp-content/themes/kawalaweb/functions.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Enqueue styles and scripts.
+ */
+function KawalaWeb_enqueue_scripts() {
+  // Load theme style.
+  wp_enqueue_style( 'KawalaWeb-style', get_stylesheet_directory_uri() . "/css/loggers.css" );
+}
+add_action( 'wp_enqueue_scripts', 'KawalaWeb_enqueue_scripts' );
