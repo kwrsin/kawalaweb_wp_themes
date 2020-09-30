da92a8be1d2c635ef4c24bdd90a3776f46ab59a6
kwrsin <kwrsin@gmail.com>
2020-09-30 11:08:59 +0900

    url links & separationg a file & show wp admin-menu.

---
+++ wordpress/html/wp-content/themes/kawalaweb/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/loggers.css">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <section id="home">
+        <figure>
+            <div>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iAxe.svg" alt="Loggers Icon" class="iloggers">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tLogger.svg" alt="Loggers" class="tloggers">
+            </div>
+            <div>
+                <figcaption>shintarou<br>kawara</figcaption>
+                <div id="avatar">
+                </div>
+            </div>
+        </figure>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#home">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iHome2.svg" alt="home" height="32px" width="32px" class="vibe">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#categories">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iCategories.svg" alt="categories" height="32px" width="32px" class="vibe">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#tags">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iTags.svg" alt="tags" height="32px" width="32px" class="vibe">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#search">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iEye.svg" alt="search" height="32px" width="32px" class="vibe">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iContact2.svg" alt="contact" height="32px" width="32px" class="vibe">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#home">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iEnter.svg" alt="KawaraWeb" height="32px" width="32px" class="vibe">
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </section>
--- wordpress/html/wp-content/themes/kawalaweb/index.php
+++ wordpress/html/wp-content/themes/kawalaweb/index.php
@@ -1,67 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kawala-Web</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/loggers.css">
-</head>
-<body>
-    <section id="home">
-        <figure>
-            <div>
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iAxe.svg" alt="Loggers Icon" class="iloggers">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tLogger.svg" alt="Loggers" class="tloggers">
-            </div>
-            <div>
-                <figcaption>shintarou<br>kawara</figcaption>
-                <div id="avatar">
-                </div>
-            </div>
-        </figure>
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#home">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iHome2.svg" alt="home" height="32px" width="32px" class="vibe">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#categories">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iCategories.svg" alt="categories" height="32px" width="32px" class="vibe">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#tags">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iTags.svg" alt="tags" height="32px" width="32px" class="vibe">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#search">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iEye.svg" alt="search" height="32px" width="32px" class="vibe">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#contact">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iContact2.svg" alt="contact" height="32px" width="32px" class="vibe">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#home">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iEnter.svg" alt="KawaraWeb" height="32px" width="32px" class="vibe">
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-
-        <!-- <div class="container">
-            <div class="articles">
-
-            </div>
-        </div> -->
-    </section>
+<?php get_header(); ?>
     <section id="contents">
         <div class="info articles_looks">
             <div class="band lastarticles">
@@ -321,5 +258,6 @@
             assemble_addr()
         })()
     </script>
+    <?php wp_footer(); ?>
 </body>
 </html>
