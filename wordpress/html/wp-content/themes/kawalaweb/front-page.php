dafe72ec2f83ade16bf2b424e389309337a3773c
kwrsin <kwrsin@gmail.com>
2020-10-02 20:27:47 +0900

    add favicon assets and setting them.

--- wordpress/html/wp-content/themes/kawalaweb/front-page.php
+++ wordpress/html/wp-content/themes/kawalaweb/front-page.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/iKawala.ico" type="image/x-icon">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
