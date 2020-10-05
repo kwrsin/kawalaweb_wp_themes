d9241fa034772143148879a4f2e7326609caa71f
kwrsin <kwrsin@gmail.com>
2020-10-05 13:51:23 +0900

    add cache busting

--- wordpress/html/wp-content/themes/kawalaweb/header.php
+++ wordpress/html/wp-content/themes/kawalaweb/header.php
@@ -4,11 +4,6 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Keywords" content="freelance,software,engineer,programming,creator,kwrsin,kawala,kawara,shintarou,job,fukuoka">
-    <?php if(is_front_page() || is_404()) { ; ?>
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
-    <?php } else { ;?>
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/loggers.css">
-    <?php } ; ?>
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/iKawala.ico" type="image/x-icon">
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/iKawala.ico" type="image/x-icon">
     <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/apple-touch-icon.png">
