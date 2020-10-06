d0f19ad56103dcd0bcfb2a7596d1982a55fe3c7c
kwrsin <kwrsin@gmail.com>
2020-10-06 19:53:43 +0900

    sending a cache-control in http header

--- wordpress/html/wp-content/themes/kawalaweb/header.php
+++ wordpress/html/wp-content/themes/kawalaweb/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> manifest="<?php echo get_template_directory_uri(); ?>/web.appcache">
+<html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
